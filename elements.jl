1c601eeed7897c3ffa38ab3fce979a38eb790e99
orblancog <oblancog@lnf.infn.it>
2020-06-30 22:52:47 +0200

    cleaning elements type

--- elements.jl
+++ elements.jl
@@ -1,10 +1,11 @@
 ### structs to create the elements
 
-### quad could be float or numeric
+### quad could be float or basic
 struct quad{T}
     k::T
     l::T
 end
+### drift could be float or basic
 struct drift{T}
     l::T
 end
