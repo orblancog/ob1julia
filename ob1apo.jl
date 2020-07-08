7f5c1fea779c654e0bc30ed27b75550e36b10519
orblancog <oblancog@lnf.infn.it>
2020-07-08 23:11:42 +0200

    creating iteration loop

--- ob1apo.jl
+++ ob1apo.jl
@@ -19,20 +19,25 @@ include("myfunctions.jl")
 dE=symbols(:dE)
 
 ### BEGIN of the script
-println("  OB1's APOCHROMAT DESIGN 2.1")
+println("  OB1's APOCHROMAT DESIGN 2.2")
 println("     author  : orblancog@gmail.com ")
 println("     release : 2020/jun/30")
 println("")
 println("    Creating the transport matrix to second order in dE")
 #R = dotransport(mlist2)
-R = dotransport(mpt104())
+
 #R = dotransport(mlist4)
 #copympt = mpt104v2D()
 #copympt[end] = [1 0; 0 1]
 #println(copympt[end])
 #R = dotransport(copympt)
 
-twisspropagation(R,0.2,0.2,0,0,1/0.2,1/0.2)
+for i in 1:2
+    @show i
+    local R = dotransport(mpt104())
+    twisspropagation(R,0.2,0.2,0,0,1/0.2,1/0.2)
+    
+end
 
 println(" Chao, 8D !!!")
 exit()
